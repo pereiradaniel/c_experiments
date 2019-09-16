84db64e9cca2f72b86ed39d7f3f81ec805a088b2
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-09-16 16:18:06 -0400

    Add variable declarations exercise.

---
+++ exercisesFromNotes/PartB_Computations/eightVariableDeclarations.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void) {
+	// Create 8 different variables, 2 of each type.
+	
+	// char
+	const char firstInitial, lastInitial;
+	
+	// int
+	int age, heightInCm;
+	
+	// float
+	float bodyFat, totalBodyWeight;
+	
+	// double
+	const double maxDeadlift, maxBackSquat;
+
+	return 0;
+}
