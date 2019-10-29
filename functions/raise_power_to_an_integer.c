839e0f6128bb2df49c71f7f86f134a35b856f81b
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-10-29 11:06:42 -0400

    Add raise to power.

---
+++ functions/raise_power_to_an_integer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// The first function returns a value of int type.
+// power identifies the function.
+// base and exponent are the function parameters.
+// both are int type.
+
+int power(int base, int exponent)
+{
+	int i, result;
+
+	result = 1;
+	for (i = 0; i < exponent; i++)
+		result = result * base;
+
+	return result;
+}
+
+int main(void)
+{
+	int base, exp, answer;
+
+	printf("Enter base : ");
+	scanf("%d", &exp);
+
+	answer = power(base, exp);
+
+	printf("%d^%d = %d\n", base, exp, answer);
+}
