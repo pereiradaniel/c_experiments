e42f6c7cedc5d5c84b014592f9b4d85f7e01b734
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-10-29 09:22:38 -0400

    Addresses added.

---
+++ addresses/addresses.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ int main(void)
+ {
+         int x;
+
+         printf("Enter x : ");
+         scanf("%d", &x);
+         printf("Value stored in x   :%d\n", x);
+         printf("Address of x        :%x\n", &x); 
+
+         return 0;
+ }
