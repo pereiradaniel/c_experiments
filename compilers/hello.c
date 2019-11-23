965263d2e37de512c2f1f62c6593c696d9baae6b
Daniel Pereira <danielpaulpereira@protonmail.com>
2019-11-23 16:54:11 -0500

    Added folder compilers, file hello.c

---
+++ compilers/hello.c
@@ -0,0 +1,11 @@
+ /* My first program          // comments introducing the source file
+    hello.c          */
+
+ #include <stdio.h>           // information about the printf identifier 
+
+ int main(void)               // the starting point of the program
+ {
+         printf("This is C"); // send output to the screen
+
+         return 0;            // return control to the operating system
+ }
