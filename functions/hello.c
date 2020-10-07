aed56f80e3e8b4f40b803aa2c011f118809c22d5
Harsh-r-c <62848594+Harsh-r-c@users.noreply.github.com>
2020-10-07 20:36:57 +0530

    Update hello.c

--- functions/hello.c
+++ functions/hello.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int hello ();
-int main (void) {
+int main (void) 
+{
 	hello();
 	return 0;
 }
-
-int hello () {
-	return printf("Hello!");
+void hello() 
+{
+	printf("Hello!");
 }
